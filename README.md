9392f45600a3db8bed23acf98149723d75e14746
penn <penn201500@gmail.com>
2017-09-07 15:52:19 +0800

    first django project, polls

---
+++ README.md
@@ -0,0 +1,17 @@
+#installation of django
+```shell
+sudo pip install django
+```
+#start project and application
+```shell
+django-admin startproject mysite
+
+python manage.py startapp polls
+
+python manage.py makemigrations polls
+
+python manage.py migrate
+```
+#reference
+http://www.cnblogs.com/fnng/p/4855743.html#3776086
+https://stackoverflow.com/questions/44320358/templatedoesnotexist-error-python-3-6-1-django-1-11-1-pycharm
---
+++ mysite/polls/views.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.all()
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'results.html', {'question': question})
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = p.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': p,
+            'error_message': "you didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (p.id,)))
